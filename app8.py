26682261442d8e52da0afbca63469f294c6dec74
jakmos138 <jakub.moszynski138@gmail.com>
2024-05-26 16:22:51 +0200

    final version

--- app8.py
+++ app8.py
@@ -4,6 +4,8 @@ import os
 import re
 import csv
 from datetime import datetime
+import openpyxl
+from openpyxl.styles import Font
 
 # URL of the dataset
 # URL: https://www.kaggle.com/datasets/vishnurajyadav12/movies-dataset-tmdb
@@ -60,19 +62,25 @@ def validate_output_file(filename):
 def parse_csv_file(filename):
     movies = []
     with open(filename, newline='', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        reader = csv.reader(
+            csvfile,
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_MINIMAL
+        )
         next(reader)  # Skip the header row
         for row in reader:
             try:
-                id = row[0]
+                id = int(row[1])
                 title = row[2]
                 overview = row[3]
                 release_date = datetime.strptime(row[4], '%Y-%m-%d')
-                popularity = row[5]
-                vote_average = row[6]
-                vote_count = row[7]
-                
-                entry = movie(id, title, overview, release_date, popularity, vote_average, vote_count)
+                popularity = float(row[5])
+                vote_average = float(row[6])
+                vote_count = int(row[7])
+                entry = movie(id, title, overview,
+                              release_date, popularity,
+                              vote_average, vote_count)
                 movies.append(entry)
             except (IndexError, ValueError) as e:
                 print("Error parsing row:", row)
@@ -80,11 +88,79 @@ def parse_csv_file(filename):
     return movies
 
 
+def average_vote_count(movies):
+    # Calculate the average vote count for all movies in the dataset.
+    total_vote_count = sum(movie.voteCount for movie in movies)
+    average = total_vote_count / len(movies)
+    return average
+
+
+def group_by_release_year(movies):
+    # Calculate the total number of movies released each year.
+    movies_by_year = {}
+    for movie in movies:
+        release_year = movie.releaseDate.year
+        if release_year not in movies_by_year:
+            movies_by_year[release_year] = 1
+        else:
+            movies_by_year[release_year] += 1
+    return movies_by_year
+
+
+def total_movies_and_popularity(movies):
+    # Calculate the the total popularity score of all movies combined.
+    total_movies = len(movies)
+    total_popularity = sum(movie.popularity for movie in movies)
+    return total_movies, total_popularity
+
+
 def run():
     args = parse_arguments()
     validate_file(args.dataset)
     validate_output_file(args.output)
     movies = parse_csv_file(args.dataset)
+
+    # Perform operations
+    avg_vote_count = average_vote_count(movies)
+    movies_by_year = group_by_release_year(movies)
+    total_movies, total_popularity = total_movies_and_popularity(movies)
+
+    # Save results to Excel file if output file is provided
+    if args.output:
+        wb = openpyxl.Workbook()
+        ws = wb.active
+        ws.title = "Movie Report"
+
+        # Write results to Excel file
+        ws['A1'] = "Average Vote Count:"
+        ws['A2'] = avg_vote_count
+
+        ws['D1'] = "Movies by Release Year:"
+        for i, (year, count) in enumerate(movies_by_year.items(), start=2):
+            ws[f'D{i}'] = year
+            ws[f'E{i}'] = count
+
+        ws['A4'] = "Total Movies:"
+        ws['A5'] = total_movies
+
+        ws['A7'] = "Total Popularity Score:"
+        ws['A8'] = total_popularity
+
+        # Apply text formatting
+        header_cells = ['A1', 'D1', 'A4', 'A7']
+        for cell in header_cells:
+            ws[cell].font = Font(name='Arial', size=12, bold=True, color="ff4444aa")
+
+        # Save Excel file
+        wb.save(args.output)
+        print(f"Results saved to {args.output}")
+
+    # Display summary results if output file is not provided
+    else:
+        print("Average Vote Count:", avg_vote_count)
+        print("Movies by Release Year:", movies_by_year)
+        print("Total Movies:", total_movies)
+        print("Total Popularity Score:", total_popularity)
 
 
 if __name__ == "__main__":
