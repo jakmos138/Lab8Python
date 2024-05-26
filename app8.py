43a7cfaedf31543ad0db9021f285f996560b8722
jakmos138 <jakub.moszynski138@gmail.com>
2024-05-26 16:04:05 +0200

    Correcting the .csv file inconsistencies

--- app8.py
+++ app8.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 import os
 import re
+import csv
+from datetime import datetime
 
 # URL of the dataset
 # URL: https://www.kaggle.com/datasets/vishnurajyadav12/movies-dataset-tmdb
@@ -56,32 +58,26 @@ def validate_output_file(filename):
 
 
 def parse_csv_file(filename):
-    entries = []
-    with open(filename, encoding="utf-8") as file:
-        # Regular expression pattern to capture groups and omit quotes
-        pattern = re.compile(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''')
-
-        # Find all matches using the pattern
-        for line in file.readlines()[1:-1]:
-            matches = pattern.findall(line)
-            # Strip spaces and quotes from matches
-            matches = [match.strip(' "') for match in matches]
+    movies = []
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        next(reader)  # Skip the header row
+        for row in reader:
             try:
-                entry = movie(
-                    id=matches[1],
-                    title=matches[2],
-                    overview=matches[3],
-                    releaseDate=matches[4],
-                    popularity=matches[5],
-                    voteAverage=matches[6],
-                    voteCount=matches[7]
-                )
-                entries.append(entry)
-            except IndexError:
-                print("Error parsing line:", line)
-                entries.append(None)
-
-    return entries
+                id = row[0]
+                title = row[2]
+                overview = row[3]
+                release_date = datetime.strptime(row[4], '%Y-%m-%d')
+                popularity = row[5]
+                vote_average = row[6]
+                vote_count = row[7]
+                
+                entry = movie(id, title, overview, release_date, popularity, vote_average, vote_count)
+                movies.append(entry)
+            except (IndexError, ValueError) as e:
+                print("Error parsing row:", row)
+                print("Exception:", e)
+    return movies
 
 
 def run():
